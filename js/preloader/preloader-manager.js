462f2671b586851c78dd68e50c03acd1e59cfb41
Theo-Sempaii <emanuel250gameryt@gmail.com>
2021-11-30 01:40:33 -0300

    added center image

--- js/preloader/preloader-manager.js
+++ js/preloader/preloader-manager.js
@@ -1,22 +1,49 @@
 'use strict';
 
-const preBase = document.getElementById('preloader-base');
+const preloaderBase = document.getElementById('preloader-base');
+const preloaderImage = document.getElementById('preloader-image')
+window.onload = main
 
-preBase.style.transitionDuration = '2s'
-preBase.style.backgroundColor = 'rgba(255, 255, 255, 0)'
+translatePreLoaderImage()
 
-movePreLoaderBars()
 
-setTimeout(() => changeTitle(), 2000);
+
+function main() {
+  document.title = "TheoSempaii's portfolio"
+  preloaderBase.style.transitionDuration = '2s'
+  preloaderBase.style.backgroundColor = 'rgba(255, 255, 255, 0)'
+  clearPreloader()
+}
+
+
+function createPreLoaderBaseParticles() {
+
+}
+
+function translatePreLoaderImage() {
+  setInterval(() => {
+    const deg = !preloaderImage.style.background.includes('deg') ? 180 : preloaderImage.style.background.split('deg')[0].split('(')[1]
+    preloaderImage.style.background = `linear-gradient(${deg < 360 ? Number(deg) + 5 : 0}deg, red, rgb(255, 136, 0))`
+  }, 10);
+}
+
+function clearPreloader() {
+  preloaderImage.style.transitionDuration = '0.8s'
+  preloaderImage.style.transitionTimingFunction = 'ease-in-out';
+  preloaderImage.style.width = '40%'
+  setTimeout(() => {
+    preloaderImage.style.width = '0%'
+    preloaderImage.style.padding = '0%'
+  }, 800);
+  movePreLoaderBars()
+}
+
+
 
 function movePreLoaderBars() {
   const bar = document.getElementById(`preloader-left`)
-  bar.style.transitionDuration = `${window.innerWidth / 200}s`
+  bar.style.transitionDuration = `${Math.round(window.innerWidth / 200)}s`
   bar.style.transitionTimingFunction = 'ease-in-out'
   bar.style.marginRight = '100%'
-  preBase.style.transform = 'rotate(120deg)'
-}
-
-function changeTitle() {
-  document.title = "TheoSempai's portafolio"
+  preloaderBase.style.transform = 'rotate(120deg)'
 }
