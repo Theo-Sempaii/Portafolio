812fb80f2f7c0d1831366b5a5e1d345056d58138
Theo-Sempaii <emanuel250gameryt@gmail.com>
2021-12-31 01:29:41 -0300

    dark mode and light mode

--- js/preloader/preloader-manager.js
+++ js/preloader/preloader-manager.js
@@ -7,43 +7,60 @@ window.onload = main
 
 translatePreLoaderImage()
 
-
-
 function main() {
   document.title = "TheoSempaii's portfolio"
   preloaderBase.style.transitionDuration = '2s'
   preloaderBase.style.backgroundColor = 'rgba(255, 255, 255, 0)'
-  clearPreloader()
+  clearPreloader(false)
 }
 
 
 function translatePreLoaderImage() {
-  setInterval(() => {
+  const interval = setInterval(() => {
     const deg = !preloaderImage.style.background.includes('deg') ? 180 : preloaderImage.style.background.split('deg')[0].split('(')[1]
     preloaderImage.style.background = `linear-gradient(${deg < 360 ? Number(deg) + 5 : 0}deg, rgb(68, 0, 255), rgb(0, 174, 255))`
   }, 10);
+  setTimeout(() => {
+    clearInterval(interval)
+  }, 10000)
 }
 
 function clearPreloader() {
   preloaderImage.style.transitionDuration = `1s`
   preloaderImage.style.transitionTimingFunction = 'ease-in-out';
+
   preloaderImage.style.width = '40%'
   setTimeout(() => {
     preloaderImage.style.width = '0%'
     preloaderImage.style.padding = '0%'
   }, 800);
   movePreLoaderBars()
-  setTimeout(()=>{
-    container.remove()
-  }, 5000)
+  var val = 0;
+  if (window.innerWidth < 1280) val = 2500
+  else if (window.innerWidth < 1920) val = 3000
+  else if (window.innerWidth < 2560) val = 3500
+  else val = 10000
+  setTimeout(() => {
+    preloaderBase.style.transitionDuration = '0s'
+    container.style.transitionDuration = '0s'
+    container.style.height = '0%'
+    container.style.width = '0%'
+    preloaderBase.style.height = '0%'
+    preloaderBase.style.width = '0%'
+  }, val)
+
+
 }
 
 
 
 function movePreLoaderBars() {
+
   const bar = document.getElementById(`preloader-left`)
   bar.style.transitionDuration = `${Math.round(window.innerWidth / 200)}s`
   bar.style.transitionTimingFunction = 'ease-in-out'
   bar.style.marginRight = '100%'
   preloaderBase.style.transform = 'rotate(120deg)'
+
+
 }
--- js/time.js
+++ js/time.js
@@ -1,19 +1,83 @@
 const hour = new Date().getHours()
 
-const swipper = document.getElementsByClassName('swiper')[0]
-const header = document.getElementById('header')
-const footer = document.getElementById('footer')
 const preloaderLeft = document.getElementById('preloader-left')
 const preloaderRight = document.getElementById('preloader-right')
 const preloaderImg = document.getElementById('plimg')
+const timeIcon = document.getElementById('time-icon')
 
-if (hour > 19 || hour < 6) {
-  swipper.classList.replace('day-mode', 'night-mode')
-  header.classList.replace('day-mode', 'night-mode')
-  footer.classList.replace('day-mode', 'night-mode')
-  preloaderLeft.style.background = 'linear-gradient(0deg, rgb(22, 59, 50), rgb(61, 1, 88))'
-  preloaderRight.style.background = 'linear-gradient(0deg, rgb(22, 59, 50), rgb(61, 1, 88))'
-  preloaderImg.style.background = 'rgb(29, 29, 61)'
+if (hour >= 19 || hour <= 6) changeTime(false)
+else changeTime(true)
+
+/**
+ * 
+ * @param {Boolean} day 
+ */
+function changeTime(day) {
+
+  if (!day) {
+    [...document.getElementsByClassName('day-mode')].forEach(element => {
+      element.classList.replace('day-mode', 'night-mode')
+    });
+
+    preloaderLeft.style.background = 'linear-gradient(0deg, rgb(22, 59, 50), rgb(61, 1, 88))'
+    preloaderRight.style.background = 'linear-gradient(0deg, rgb(22, 59, 50), rgb(61, 1, 88))'
+    preloaderImg.style.background = 'rgb(29, 29, 61)'
+    timeIcon.src = 'img/moon.png'
+    timeIcon.className = 'nm';
+
+  } else {
+    timeIcon.src = 'img/sun.png';
+    timeIcon.className = 'dm';
+    [...document.getElementsByClassName('night-mode')].forEach(element => {
+      element.classList.replace('night-mode', 'day-mode')
+    });
+
+  }
+
+
 }
 
-console.log(hour)
+timeIcon.addEventListener('click', () => {
+
+  const nightMode = timeIcon.className == 'nm' ? true : false;
+  const transitionContainer = document.createElement('div')
+  transitionContainer.className = nightMode ? 'transitionContainer bgIN' : 'transitionContainer bgID'
+  transitionContainer.style.opacity = '0'
+  document.body.insertAdjacentElement('afterbegin', transitionContainer)
+
+  function opacityManagerIn() {
+    setTimeout(() => {
+
+      if (transitionContainer.style.opacity < 1) {
+        transitionContainer.style.opacity = String(Number(transitionContainer.style.opacity) + 0.01)
+        opacityManagerIn()
+      } else {
+        transitionContainer.className = nightMode ? 'transitionContainer bgTIN' : 'transitionContainer bgTID'
+
+        setTimeout(() => { opacityManagerOut(); changeTime(nightMode) }, 1000)
+      }
+    }, 1);
+  }
+
+  function opacityManagerOut() {
+    setTimeout(() => {
+      if (transitionContainer.style.opacity > 0) {
+        transitionContainer.style.opacity = String(Number(transitionContainer.style.opacity) - 0.01)
+        opacityManagerOut()
+      } else {
+        setTimeout(() => transitionContainer.remove(), 1000)
+      }
+    }, 10)
+  }
+
+  opacityManagerIn()
+})
+
+
+
+
+
+
+
+
+
